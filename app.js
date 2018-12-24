5b686deb432fadd2a4097b819f31fd1d5c32045a
Thabiso <pragolan@student.wethinkcode.co.za>
2018-12-25 00:16:28 +0200

    another push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const path = require('path');
 
 var app = express();
