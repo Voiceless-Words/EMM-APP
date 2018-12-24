379cb285db54d7ae55fcb0422ddbd294823aa7aa
graphixxx <swilson@student.wethinkcode.co.za>
2018-12-24 21:36:27 +0200

    environment setup

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+var app = express();
+var server = app.listen(8080, function(){
+    // console.log("server started port 5200");
+})
+
+const user = require('./routes/user');
+
+
+
+//session middleware
+ app.use(session({
+     secret: "passwhatever12", resave: false, saveUninitialized: true
+ }));
+
+//view engine middleware
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//ser static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//use user
+app.use('/user', user);
+
+app.use(function(req, res, next){
+    errors = [];
+    next();
+});
+
+
+app.use(function(req, res) {
+    res.send("what are you trying to do" + req.url);
+});
