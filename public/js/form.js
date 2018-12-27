ade15b6a3e6a00649bbbd1ede722b4951c1e46bd
Thabiso <pragolan@student.wethinkcode.co.za>
2018-12-27 21:58:01 +0200

    independent

--- public/js/form.js
+++ public/js/form.js
@@ -1,22 +1,56 @@
-
+var i = 1;
+function add(){
+  $('.cableNumber').html(i);
+  $('#meterA'+(i - 1)).attr('class','meterAvailable'+ i);
+  $('#meterCheck'+(i - 1)).attr('id','meterCheck'+ i);
+  $('#cable'+(i - 1)).attr('id','cable'+ i);
+  $('#meterA'+(i - 1)).attr('id','meterA'+ i);
+  $('#delete' +(i - 1)).attr('id', 'delete' + i);
+  $('.dynamic-element').first().clone().appendTo('.dynamic-stuff').show();
+  attach_delete();
+  i++;
+}
 
 //Attach functionality to delete buttons
-function attach_delete(){
+function attach_delete(eleId){
+
   $('.delete').off();
   $('.delete').click(function(){
     console.log("click");
-    $(this).closest('.form-group').remove();
+    var k = 0;
+    var l = "";
+    $string = $(this).attr('id');
+    while ($string[k]) {
+      if ($string[k] >= '1' && $string[k] <= '9'){
+        l = l + $string[k];
+      }
+      k++;
+    }
+    $(this).closest('#cable' + l).remove();
+    if(i !== 1)
+    {
+      i--;
+    }
   });
 }
 
 function meterSelectCheck(nameSelect)
 {
     var val = nameSelect.value;
+    var string = nameSelect.id;
+    var k = 0;
+    var l = "";
+    while (string[k]) {
+      if (string[k] >= '1' && string[k] <= '9'){
+        l = l + string[k];
+      }
+      k++;
+    }
     if (val === "NA")
     {
-      $(".meterAvailable").hide();
+      $(".meterAvailable" + l).hide();
     }
     else {
-        $(".meterAvailable").show();
+        $(".meterAvailable" + l).show();
     }
 }
